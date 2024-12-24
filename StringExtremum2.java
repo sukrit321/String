2ca358b62f64feb59d32ecd67086480cd4dcd9dc
sukrit321 <54735307+sukrit321@users.noreply.github.com>
2024-12-24 14:44:58 +0700

    Add files via upload

--- StringExtremum2.java
+++ StringExtremum2.java
@@ -4,6 +4,7 @@ public class StringExtremum2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        sc.nextLine();
         String taget1 = sc.nextLine();
         String taget2 = sc.nextLine();
         int max = Integer.MIN_VALUE;
@@ -18,17 +19,18 @@ public class StringExtremum2 {
             }
             if(num == 1){
                 if(n.indexOf(taget1) >= 0 || n.indexOf(taget2) >= 0){
-                    System.out.println(n.indexOf(taget1));
+                    // System.out.println(n.indexOf(taget1));
+                    //System.out.println(n.indexOf(taget2));
                     if(n.length() >= max){
-                       max = n.length();
-                       a=n;
+                        max = n.length();
+                        a=n;
                     }
                     // System.out.println(max);
                 }
             }
             else {
                 if(n.indexOf(taget1) >= 0 || n.indexOf(taget2) >= 0){
-                    System.out.println(n);
+                    // System.out.println(n);
                     if(n.length() <= min){
                         min = n.length();
                         a = n;
